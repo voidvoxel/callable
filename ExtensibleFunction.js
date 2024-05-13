5e65b5da620cdb33726e5f8894195b642ed42a7d
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-13 10:48:59 -0400

    Add class `ExtensibleFunction`

---
+++ ExtensibleFunction.js
@@ -0,0 +1,11 @@
+class ExtensibleFunction extends Function {
+    constructor (f) {
+        return Object.setPrototypeOf(
+            f,
+            new.target.prototype
+        );
+    }
+}
+
+
+module.exports = ExtensibleFunction;
